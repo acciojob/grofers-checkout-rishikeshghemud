67665ee2cdedc4eca4d5ba3fb93794aa27e31f3e
Rishikesh <146565427+rishikeshghemud@users.noreply.github.com>
2025-11-06 01:27:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,32 +3,21 @@ getSumBtn.append("Get Total Price");
 document.body.appendChild(getSumBtn);
 
 const getSum = () => {
-//Add your code here
-	const row = document.createElement('tr');
-const totalText = document.createElement('td');
-totalText.innerHTML = '<b>Total</b>';
-const totalAmount = document.createElement('td');
+  const row = document.createElement('tr');
+  const totalText = document.createElement('td');
+  totalText.innerHTML = '<b>Total</b>';
+  const totalAmount = document.createElement('td');
 
-row.appendChild(totalText);
-row.appendChild(totalAmount);
+  const table = document.querySelector('table');
+	
+  const totalSum = Array.from(document.getElementsByClassName('price'))
+    .map(x => Number(x.textContent.replace(/[^\d]/g, '') || 0))
+    .reduce((acc, curr) => acc + curr, 0);
 
-const table = document.querySelector('table');
-
-
-const totalSum = Array.from(document.getElementsByClassName('price'))
-  .map(x => {
-	const matches = x.textContent.match(/\d+/g);
-  return matches ? Number(matches[matches.length - 1]) : 0;  
-  })
-  .reduce((acc, curr) => {
-    return acc + curr
-  }, 0)
-;
-
-totalAmount.innerHTML = `<b id="ans">${totalSum}</b>`;
-
-table.appendChild(row)
-  
+  totalAmount.innerHTML = `<b id="ans">${totalSum}</b>`;
+  row.appendChild(totalText);
+  row.appendChild(totalAmount);
+  table.appendChild(row);
 };
 
 getSumBtn.addEventListener("click", getSum);
